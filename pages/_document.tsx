7ec628c9a74f878922fd47d9cadde731e67a68d7
Manuel Vega <manuelvega.dev@gmail.com>
2023-04-24 01:29:19 -0500

    feature(project): Creating the styles (WIP)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+        <title>Manuel Vega - FullStack Web Dev (WIP)</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
